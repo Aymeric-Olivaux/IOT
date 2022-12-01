16dd249369f250c6fdefcef2ef4837fb44b015b8
Milann Guitton <milann.guitton@epita.fr>
2022-12-01 15:41:41 +0100

    [UPDATE][DATA] Close values send after each other

--- database/data.sh
+++ database/data.sh
@@ -4,8 +4,17 @@ MAX=110
 MIN=40
 
 while true; do
+
+
     VALUE=$(($MIN + $RANDOM % $MAX))
-    CMD=$(echo localhost:8000/data -d "'"{ \"device_id\": 1, \"decibels\": ' '$VALUE' '}"'" "-H 'Content-Type: application/json'")
-    echo "$CMD" | xargs curl
-    sleep 5
+    NUMBER=10
+    while [ $NUMBER -gt 0 ]; do
+        NUMBER=$((NUMBER - 1))
+        DELTA=$((-10 + $RANDOM % 10))
+        DECIBELS=$(($DELTA + $VALUE))
+        CMD=$(echo localhost:8000/data -d "'"{ \"device_id\": 1, \"decibels\": ' '$DECIBELS' '}"'" "-H 'Content-Type: application/json'")
+        echo "$CMD" | xargs curl
+        sleep 5
+    done
+
 done
