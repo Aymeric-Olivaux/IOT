fe997197e0f7ac33b34f2446fbb0e7ac9353d79e
Milann Guitton <milann.guitton@epita.fr>
2022-12-01 15:47:55 +0100

    [ADD][BACKEND] Routes for minute and hour display

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -2,6 +2,8 @@ from sqlalchemy.orm import Session
 
 from . import models, schemas
 
+import datetime
+
 def get_user(db: Session, email: str, password: str):
     return db.query(models.User).filter(models.User.email == email, models.User.password == password).first()
 
@@ -16,8 +18,16 @@ def create_user(db: Session, user: schemas.UserCreate):
     return db_user
 
 def insert_data(db: Session, data: schemas.DataCreate):
-    db_data = models.Data(device_id = data.device_id, decibels = data.decibels)
+    db_data = models.Data(device_id = data.device_id, collected_at=datetime.datetime.now(), decibels = data.decibels)
     db.add(db_data)
     db.commit()
     db.refresh(db_data)
     return db_data
+
+def fetch_data_minute(db: Session, device_id: int):
+    last_minute = datetime.datetime.now() - datetime.timedelta(minutes=1)
+    return db.query(models.Data).filter(models.Data.device_id == device_id, models.Data.collected_at >= last_minute).all()
+
+def fetch_data_hour(db: Session, device_id: int):
+    last_hour = datetime.datetime.now() - datetime.timedelta(hours=1)
+    return db.query(models.Data).filter(models.Data.device_id == device_id, models.Data.collected_at >= last_hour).all()
--- backend/app/main.py
+++ backend/app/main.py
@@ -5,7 +5,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from .database import SessionLocal, engine
 from sqlalchemy.orm import Session
 from . import models, schemas
-from .crud import get_user, get_user_by_email, create_user, insert_data
+from .crud import get_user, get_user_by_email, create_user, insert_data, fetch_data_minute, fetch_data_hour
+from datetime import datetime
+import logging
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -51,3 +53,29 @@ def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
 def post_data(data: schemas.DataCreate, db: Session = Depends(get_db)):
     db_data = insert_data(db, data)
     return db_data
+
+@app.get("/data/{device_id}/minute")
+def get_data_minute(device_id: int, db: Session = Depends(get_db)):
+    data =  fetch_data_minute(db, device_id)
+    decibels_data = []
+    time_data = []
+    for d in data:
+        decibels_data.append(d.decibels)
+        t = d.collected_at
+        time_data.append(t.strftime("%H:%M:%S"))
+
+    return {"decibels": decibels_data, "time": time_data}
+
+@app.get("/data/{device_id}/hour")
+def get_data_minute(device_id: int, db: Session = Depends(get_db)):
+    data =  fetch_data_hour(db, device_id)
+    decibels_data = []
+    time_data = []
+    prev = 0
+    length = len(data)
+    for i in range(50, length, 50):
+        decibels_data.append(sum(d.decibels for d in data[prev:i])/50)
+        t = data[prev + 25].collected_at
+        time_data.append(t.strftime("%H:%M"))
+        prev = i
+    return {"decibels": decibels_data, "time": time_data}
