dbf67f09124bb77fe93a0518580b17912df726b0
Milann Guitton <milann.guitton@epita.fr>
2022-12-09 15:14:48 +0100

    [FIX][SQL] Threshold table name

--- database/02_threshold.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS threshold;
-
-CREATE TABLE threshold (
-    id SERIAL PRIMARY KEY,
-    device_id INTEGER REFERENCES devices(id) ON DELETE CASCADE,
-    threshold INTEGER
-);
-
-INSERT INTO threshold (device_id, threshold) VALUES (1, 50);
