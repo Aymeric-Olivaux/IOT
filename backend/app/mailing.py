22583afb28b59b6ea24048bc1ad30a7f1877dd9c
Milann Guitton <milann.guitton@epita.fr>
2022-12-15 12:08:35 +0100

    [UPDATE][MAILING] Get smpt server address from env

--- backend/app/mailing.py
+++ backend/app/mailing.py
@@ -1,11 +1,12 @@
 import smtplib, ssl
 import os
 from dotenv import load_dotenv
-import logging
+import datetime
 
 port = 465  # For SSL
 
 load_dotenv()
+smtp_server = os.getenv('SMTP_SERVER')
 sender_address = os.getenv('EMAIL_ADDRESS')
 password = os.getenv('EMAIL_PASSWORD')
 
@@ -13,9 +14,8 @@ password = os.getenv('EMAIL_PASSWORD')
 context = ssl._create_unverified_context()
 
 def send_email(email, message):
-    content = f'Subject: Ambizen report\n\nThe last value recorded by your device is {message}'
-
-    with smtplib.SMTP_SSL("mail.tryhard.fr", context=context) as server:
-        logging.getLogger('mailing').warning('Sending email to ' + email)
+    date = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    content = f'Subject: Ambizen report - {date}\n\nThe last value recorded by your device is {message}'
+    with smtplib.SMTP_SSL(smtp_server, context=context) as server:
         server.login(sender_address, password)
         server.sendmail(sender_address, email, content)
